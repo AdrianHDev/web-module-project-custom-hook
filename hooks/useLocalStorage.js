e0030a0aaef8c78f53ebdb574f8c2ba51083e1ff
AdrianHDev <adrianhensondev@gmail.com>
2021-06-02 12:44:18 -0400

    Create useLocalStorage hook

---
+++ hooks/useLocalStorage.js
@@ -0,0 +1,13 @@
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const lsItem = window.localStorage.getItem(key);
+    return lsItem ? JSON.parse(lsItem) : initialValue;
+  });
+  const setValue = (value) => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage;
